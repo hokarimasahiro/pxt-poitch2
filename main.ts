1c3d0223fd51974d0010789231372ebf20376eb7
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-11-07 05:18:51 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,7 @@ namespace poitch2 {
 
     function initSwitch(): void {
         pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
-        pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
     }
