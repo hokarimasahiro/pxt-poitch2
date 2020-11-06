599ecc21b83282a98bc2f7bac756d69f5fd5939a
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-11-07 05:21:21 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,19 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+music.rest(music.beat(BeatFraction.Whole))
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        music.ringTone(262)
+    } else if (poitch2.testSwitch(DigitalPin.P8)) {
+        music.ringTone(294)
+    } else if (input.pinIsPressed(TouchPin.P1)) {
+        music.ringTone(330)
+    } else if (poitch2.testSwitch(DigitalPin.P12)) {
+        music.ringTone(349)
+    } else if (poitch2.testSwitch(DigitalPin.P13)) {
+        music.ringTone(392)
+    } else if (input.buttonIsPressed(Button.B)) {
+        music.ringTone(440)
+    } else {
+        basic.clearScreen()
+        music.rest(music.beat(BeatFraction.Sixteenth))
+    }
+})
