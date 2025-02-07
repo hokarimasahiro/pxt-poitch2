5ac5b11b6e17918499b126dcf0051e552ec0b112
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-02-08 03:25:31 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ namespace poitch2 {
     let initFlag = 0;
 
     function initSwitch(): void {
-        pins.digitalWritePin(DigitalPin.P2, 1);
-        pins.setPull(DigitalPin.P8, PinPullMode.PullDown);
-        pins.setPull(DigitalPin.P12, PinPullMode.PullDown);
-        pins.setPull(DigitalPin.P13, PinPullMode.PullDown);
+        pins.digitalWritePin(DigitalPin.P2, 0);
+        pins.setPull(DigitalPin.P8, PinPullMode.PullUp);
+        pins.setPull(DigitalPin.P12, PinPullMode.PullUp);
+        pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
         initFlag=1;
     }
     /**
@@ -18,10 +18,21 @@ namespace poitch2 {
 		if (initFlag == 0){
 			initSwitch();
 		}
-        if (pins.digitalReadPin(sw)==1){
+        if (pins.digitalReadPin(sw)==0){
             return true;
         } else {
             return false;
         }
+    }
+    /**
+     * get switch data
+     * @param sw read pin of switch
+     */
+    //% blockId=readSwitch block="read switch at|%sw"
+    export function readSwitch(sw: DigitalPin): number {
+        if (initFlag == 0) {
+            initSwitch();
+        }
+        return pins.digitalReadPin(sw);
     }
 }
